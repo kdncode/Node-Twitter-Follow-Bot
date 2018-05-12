49b21c56aead7ae3c1db976d77d094e959f25ff0
kdncode <kdncode@gmail.com>
2018-05-11 22:28:45 -0500

    Authetication successfully

---
+++ index.js
@@ -0,0 +1,19 @@
+const Twit = require('twit');
+
+var T = new Twit({
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
+})
+
+T.get('account/verify_credentials', {
+    include_entities: false,
+    skip_status: true,
+    include_email: false
+}, onAuthenticated)
+
+function onAuthenticated(err, res) {
+    if (err) { throw err }
+    console.log('Authentication successful. Running bot...\r\n')
+}
